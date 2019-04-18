84f5b8fa8e9036d71db347ed4e4468e041d615eb
BuildTools <unconfigured@null.spigotmc.org>
2019-04-18 05:26:06 +0200

    added some prop sprites

--- Assets/Scripts/Player/SanityController.cs
+++ Assets/Scripts/Player/SanityController.cs
@@ -6,27 +6,66 @@ using UnityEngine.Rendering.PostProcessing;
 public class SanityController : MonoBehaviour
 {
     [SerializeField]
-    private GameObject _postProcessing;
+    public GameObject postProcessing;
 
     private PostProcessProfile _postProcessProfile;
     private Vignette _vignette;
+
+    private bool _enabled;
     
-    private int _sanity = 100;
+    [Range(0f,100f)]
+    public float _sanity = 100;
+    
+    private float _lastKnowSanity = 100f;
+
+    [Range(0f,1f)]
+    public float minimalShade;
+    
+    [Range(0f,1f)]
+    public float maximalShade;
+
+    private float _shadeModifier;
+    
     
     // Start is called before the first frame update
     void Start()
     {
-        var volume = this._postProcessing.GetComponent<PostProcessVolume>();
+        PostProcessVolume volume = this.postProcessing.GetComponent<PostProcessVolume>();
         
         this._postProcessProfile = volume.profile;
-        this._vignette = this._postProcessProfile.GetSetting<Vignette>();
+
+        if (this._postProcessProfile == null)
+        {
+            this._enabled = false;
+            return;
+        }
+
+        this._enabled = this._postProcessProfile.TryGetSettings<Vignette>(out _vignette);
+
+        if (this._enabled)
+        {
+            this._vignette.intensity.value = this.minimalShade;
+
+            float totalShadeAmmount = this.maximalShade - this.minimalShade;
+            this._shadeModifier = totalShadeAmmount / 100;
+        }
     }
 
-    // Update is called once per frame
+    // Update is called once per framewwww
     void Update()
     {
-        this._vignette.intensity.SetValue()
-      
-        Debug.Log("test");
+        if (!this._enabled)
+        {
+            return;
+        }
+
+        if (this._lastKnowSanity == this._sanity)
+        {
+            return;
+        }
+        
+        this._vignette.intensity.value = this._shadeModifier * (100f - this._sanity);
+
+        this._lastKnowSanity = this._sanity;
     }
 }
