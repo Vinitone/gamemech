095434993f9df97aa93b3bc5ab2a62296fdb46c4
BuildTools <unconfigured@null.spigotmc.org>
2019-04-18 02:13:36 +0200

    added ground sprites

---
+++ Assets/Scripts/Player/PlayerEconomy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerEconomy : MonoBehaviour
+{
+    public int coins;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.coins = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
