7f20a6ca560082327692cc44044e8a7cbe929da9
Vincent de Vries <Vincent.de.Vries3@hva.nl>
2019-04-18 22:19:48 +0200

    Added soul currency

---
+++ Assets/Scripts/SoulScore.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoulScore : MonoBehaviour
+{
+    Text text;
+    private int soulAmount = 0;
+
+    private static SoulScore instance = null;
+     
+     // Game Instance Singleton
+     public static SoulScore Instance
+     {
+         get
+         { 
+             return instance; 
+         }
+     }
+     
+     private void Awake()
+     {
+         // if the singleton hasn't been initialized yet
+         if (instance != null && instance != this) 
+         {
+             Destroy(this.gameObject);
+         }
+ 
+         instance = this;
+         DontDestroyOnLoad( this.gameObject );
+     }
+    public int SoulAmount
+    {
+        
+        get{
+            return soulAmount;
+        } 
+        set{
+            soulAmount = value;
+        }
+    }
+    void Start()
+    {
+        text = gameObject.GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = soulAmount.ToString();
+    }
+}
---
+++ Assets/Scripts/SoulScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoulScript : MonoBehaviour
+{
+    
+    public GameObject soulCatcher;
+    private void OnTriggerEnter2D(Collider2D other) {
+        
+        
+
+
+        StartCoroutine(SoulFloat(soulCatcher));
+    }
+
+    private IEnumerator SoulFloat(GameObject obj)
+    {
+        	//Get the positions
+		Vector3 startingPos = this.transform.position;
+		Vector3 endPos = obj.transform.position;
+
+		//Get the timers
+		float duration = 1;
+		float elapsedTime = 0f;
+
+		//Get the sizes
+
+
+		while (elapsedTime < duration) {
+			transform.position = Vector3.Lerp (startingPos, endPos, (elapsedTime / duration));
+			elapsedTime += Time.deltaTime;
+            endPos = obj.transform.position;
+			yield return new WaitForEndOfFrame ();
+		}
+        SoulScore.Instance.SoulAmount++;
+        Destroy(this.gameObject);
+    }
+}
