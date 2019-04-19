25126e9d580090eb50c5c60242982d6e7d0031e6
Vincent de Vries <Vincent.de.Vries3@hva.nl>
2019-04-19 16:36:06 +0200

    Made half of the basic AI

---
+++ Assets/Scripts/SimpleAI.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SimpleAI : MonoBehaviour
+{
+    public string Name { get; private set; }
+    public float Health { get; set; }
+
+    public Animator anim;
+    private bool facingRight = true;
+    private float cooldown;
+    
+    public void Flip(Transform trnsfrm)
+    {
+        // Switch the way the player is labelled as facing.
+        facingRight = !facingRight;
+
+        // Multiply the player's x local scale by -1.
+        Vector3 theScale = trnsfrm.localScale;
+        theScale.x *= -1;
+        trnsfrm.localScale = theScale;
+    }
+
+    public void Move(Transform trnsfrm, float speed)
+    {
+        if(facingRight)
+            trnsfrm.Translate(Vector2.right * speed * Time.deltaTime);
+        else
+            trnsfrm.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+
+    public bool Cooldown(float seconds)
+    {
+
+        if (Time.time >= cooldown)
+        {
+            cooldown = Time.time + seconds;
+            return true;
+        }
+        return false;
+    }
+
+    public void Death(GameObject obj, float health)
+    {
+        if (health <= 0)
+        {
+            Destroy(obj);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            //anim.SetTrigger("Hit");
+            //collision.gameObject.GetComponent<Player>().health.Reduce(Damage);
+            
+        }
+
+    }
+}
--- Assets/Scripts/SoulScript.cs
+++ Assets/Scripts/SoulScript.cs
@@ -8,24 +8,17 @@ public class SoulScript : MonoBehaviour
     public GameObject soulCatcher;
     private void OnTriggerEnter2D(Collider2D other) {
         
-        
-
-
         StartCoroutine(SoulFloat(soulCatcher));
     }
 
     private IEnumerator SoulFloat(GameObject obj)
     {
-        	//Get the positions
+    	//Get the positions
 		Vector3 startingPos = this.transform.position;
 		Vector3 endPos = obj.transform.position;
 
-		//Get the timers
 		float duration = 1;
 		float elapsedTime = 0f;
-
-		//Get the sizes
-
 
 		while (elapsedTime < duration) {
 			transform.position = Vector3.Lerp (startingPos, endPos, (elapsedTime / duration));
